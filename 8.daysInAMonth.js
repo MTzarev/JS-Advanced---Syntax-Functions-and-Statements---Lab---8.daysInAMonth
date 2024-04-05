b67a6356d42b9276823df863092586567a21bd62
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-05 23:27:43 +0300

    Add files via upload

---
+++ 8.daysInAMonth.js
@@ -0,0 +1,8 @@
+function daysInMonth(month, year) {
+let date = new Date(year, month-1, 1)
+date.setMonth(date.getMonth()+1)
+date.setDate(0)
+    console.log(date.getDate());
+}
+daysInMonth(1, 2012);
+daysInMonth(2, 2021);
