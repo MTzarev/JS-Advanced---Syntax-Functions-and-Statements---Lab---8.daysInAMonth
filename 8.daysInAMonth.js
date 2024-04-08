49566441027f3a01e406eef993d56e13643b0dfc
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-08 23:27:48 +0300

    Update 8.daysInAMonth.js

--- 8.daysInAMonth.js
+++ 8.daysInAMonth.js
@@ -1,7 +1,7 @@
 function daysInMonth(month, year) {
-let date = new Date(year, month-1, 1)
-date.setMonth(date.getMonth()+1)
-date.setDate(0)
+let date = new Date(year, month-1, 1);
+date.setMonth(date.getMonth()+1);
+date.setDate(0);
     console.log(date.getDate());
 }
 daysInMonth(1, 2012);
